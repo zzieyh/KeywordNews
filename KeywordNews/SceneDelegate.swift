dc1ad264edd27ca25a8280f867fed9924c4282db
HyunYoung-Lee <tinahy1229@naver.com>
2022-01-29 19:02:02 +0900

    Xcode project 생성

---
+++ KeywordNews/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  KeywordNews
+//
+//  Created by 이현영 on 2022/01/29.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
---
+++ KeywordNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeywordNews
+//
+//  Created by 이현영 on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
